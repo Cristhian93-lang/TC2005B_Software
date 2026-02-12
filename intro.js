ce025fc824a87341dc7c68326a2f40d94ee63c7d
Cristhian93-lang <a01668790@tec.mx>
2026-02-12 12:54:00 -0600

    mejoras

--- intro.js
+++ intro.js
@@ -1,2 +1,5 @@
 // consola(log, info, warn,error, assert)
 console.log("hola mundo!");
+console.info("Creado en el 2009")
+console.warn("Es adictivo")
+console.error("Los tanques no deben ir atras")
