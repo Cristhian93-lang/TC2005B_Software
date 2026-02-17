37c594259f517d29a353a20d7c21c15ac0067e0f
Cristhian93-lang <a01668790@tec.mx>
2026-02-16 23:19:38 -0600

    Ejercicio 3 terminado

---
+++ Laboratorios/intro.js
@@ -0,0 +1,67 @@
+//----------Ejercicio 1--------
+/*
+let numero = parseInt(promt("Introduce un numero para generar una tabla con sus cuadrados y cubos:"));
+document.write("<h2>Ejercicio 1</h2>");
+document.write("<table border='1'>");
+document.write("<tr><th>Numero</th><th>Cuadrados</th><th>Cubo</th></tr>");
+for (let i = 1; i <= numero; i++) {
+    document.write("<tr>");
+    document.write("<td>" + i + "</td>");
+    document.write("<td>" + (i * i) + "</td>");
+    document.write("<td>" + (i * i * i) + "</td>");
+    document.write("</tr>")
+}
+document.write("</table>");
+*/
+//----------Ejercicio 2--------
+let n1 = Math.floor(Math.ramdom() * 100);
+let n2 = Math.floor(Math.random() * 100 );
+let inicio = Date.now();
+let respuesta = parseInt(promt("¿Cuanto es" + n1 + " + " + n2 + "?"));
+let fin = Date.now();
+let tiempo = (fin - inicio) / 1000;
+if (respuesta === n1 + n2) {
+    document.getElementById("test02").innerHTML = "Correcto! Tiempo: " + tiempo + " segundos";
+} else {
+    document.getElementById("test02").innerHTML = "Incorrecto. Tiempo: " + tiempo + " segundos";
+}
+
+//----------Ejercicio 3--------
+function contador(arreglo) {
+    let negativos = 0;
+    let ceros = 0;
+    let positivos = 0;
+    for (let num of arreglo) {
+        if (num < 0) {
+            negativos++;
+        } else if (num === 0) {
+            ceros++;
+        } else {
+            positivos++;
+        }
+    }
+    return { negativos: negativos, 
+             ceros: ceros, 
+             positivos: positivos 
+    };
+}
+
+console.assert(contador([-1, 0, 2]).negativos === 1);
+console.assert(contador([-1, 0, 2]).ceros === 1);
+console.assert(contador([-1, 0, 2]).positivos === 1);
+console.assert(contador([-5, -3, 0, 4, 8]).positivos === 2);
+console.assert(contador([-5, -3, 0, 4, 8]).positivos === 1);
+console.assert(contador([-5, -3, 0, 4, 8]).positivos === 2);
+
+let entrada = promt("Ingresa numeros separados por comas (ejemplo: -3,0,5,7,-2,0): ");
+let arregloUsuario = entrada.split(",").map(Number);
+let resultados = contador(arregloUsuario);
+
+document.getElementById("test03").innerHTML = 
+"<h3>Resultado Eejercicio 3</h3>" + "Negativos: " + resultados.negativos +
+"<br>Ceros: " + resultados.ceros + "<br>Positivos: " + resultados.positivos;
+
+//----------Ejercicio 4--------
+
+//----------Ejercicio 5--------
+//----------Ejercicio 6--------
