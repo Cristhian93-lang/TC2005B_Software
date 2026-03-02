db844496218cbccdb81d4d8b836835049343e4e3
Cristhian93-lang <a01668790@tec.mx>
2026-03-02 16:18:21 -0600

    Empezando el laboratorio 11 de express

--- Laboratorios/Lab8y10/app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const fs = require('fs');
-
-const sever = http.createServer((server, response) => {
-    if (request.url === '/' && request.method === 'GET') {
-        
-    }
-})
